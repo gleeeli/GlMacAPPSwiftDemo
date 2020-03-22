258e3b730a28a9adc0bdd2c94e5f8e3b55d1a9ee
ChangeStrongLeiPc <491337430@qq.com>
2020-03-22 08:46:27 +0800

    Initial Commit

---
+++ GlMacAPPSwiftDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GlMacAPPSwiftDemo
+//
+//  Created by gleeeli on 2020/3/22.
+//  Copyright © 2020 gleeeli. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GlMacAPPSwiftDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GlMacAPPSwiftDemo
+//
+//  Created by gleeeli on 2020/3/22.
+//  Copyright © 2020 gleeeli. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
